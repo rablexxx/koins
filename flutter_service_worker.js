107f4c9c98ec9ebafb425f27d90226bd3dd23335
rablexxx <49977418+rablexxx@users.noreply.github.com>
2023-04-11 20:21:06 +0300

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "27118be73299f7b83f96015bfb392966",
-"index.html": "90dbb3ccefdedf0b1a30eba7ad07dbf3",
-"/": "90dbb3ccefdedf0b1a30eba7ad07dbf3",
-"main.dart.js": "6a0c16ef75d9a4d3ff0f43826df07da1",
+"index.html": "30f908065629fe92594686f4536e59c7",
+"/": "30f908065629fe92594686f4536e59c7",
+"main.dart.js": "906e6cef7bde33028a46d28015f6bc26",
 "flutter.js": "a85fcf6324d3c4d3ae3be1ae4931e9c5",
 "favicon.png": "6b25056d3872d634ce70f2ccd5dd11da",
 "icons/Icon-192.png": "65301af14f112fc599ff0806ef243f99",
